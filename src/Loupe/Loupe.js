fe7d1a88dbfee95b3836218ff586c5d69ed39cdb
Walker Randolph Smith <walkerrandolphsmith@gmail.com>
2016-07-05 22:54:45 -0400

    Tack the mouse movement and determine where the inner and outer loupe should be.

--- src/Loupe/Loupe.js
+++ src/Loupe/Loupe.js
@@ -1,57 +1,83 @@
-import React, {Component, PropTypes} from 'react';
+import React, { Component, PropTypes } from 'react';
 
 export default class Loupe extends Component {
     static propTypes = {
         image: PropTypes.string,
-        classNames: PropTypes.arrayOf(PropTypes.string)
+        classNames: PropTypes.arrayOf(PropTypes.string),
+        width: PropTypes.number,
+        height: PropTypes.number
     };
 
     static defaultProps = {
         image: '',
-        classNames: ['']
+        classNames: [''],
+        width: 100,
+        height: 100
     };
 
     constructor(props, context) {
         super(props, context);
         this.state = {
             isShown: props.isShown,
-            top: 0,
-            letf: 0
+            big: { top: 0, left: 0 },
+            small: { top: 0, left: 0 }
         }
     }
 
     componentWillReceiveProps(nextProps) {
         if(nextProps.isShown !== this.state.isShown) {
-            this.setState({
-                isShown: nextProps.isShown
-            });
+            this.setState({ isShown: nextProps.isShown });
         }
     }
 
     onMouseEnter(event) {
-        this.setState({
-            isShown: true
-        });
+        this.setState({ isShown: true });
     }
 
     onMouseLeave(event) {
-        this.setState({
-            isShown: false
-        });
+        this.setState({ isShown: false });
     }
 
     onMouseMove(event) {
-        const x = event.clientX;
-        const y = event.clientY;
+        let { width, height } = this.props;
+        width = width / 2;
+        height = height / 2;
+        const x = event.pageX;
+        const y = event.pageY;
+        const smallElm = event.target;
+        const bigElm = smallElm.children[0];
+
+        const smallWidth = smallElm.offsetWidth;
+        const smallHeight = smallElm.offsetHeight;
+
+        const big = bigElm.getBoundingClientRect();
+        const small = smallElm.getBoundingClientRect();
+
+        const bl = -(((x - small.left) / smallWidth) * big.width - width) | 0;
+        const bt = -(((y - small.top) / smallHeight) * big.height - height) | 0;
+
+        const sl = x - width;
+        const st = y - height;
+
+        const shouldHide = (
+            x > smallWidth + small.left + 10
+            || x < small.left - 10
+            || y > smallHeight + small.top + 10
+            || y < small.top - 10
+        );
+
         this.setState({
-            top: y,
-            left: x
+            isShown: shouldHide ? false : true,
+            small: { top: st, left: sl },
+            big: { top: bt, left: bl }
         });
     }
 
     render() {
-        const { image, classNames } = this.props;
-        const { isShown, top, left } = this.state;
+        const { image, classNames, width, height } = this.props;
+        const { isShown, small, big } = this.state;
+
+        const containerClassNames = `container ${classNames.join(' ')}`;
 
         const containerStyle = {
             position: 'relative',
@@ -63,19 +89,24 @@ export default class Loupe extends Component {
             backgroundSize: 'contain'
         };
 
-        const containerClassNames = `container ${classNames.join(' ')}`;
-
-        const loupeStyle = {
+        const bigStyle = {
             position: 'absolute',
-            top: top,
-            left: left,
-            zIndex: '2',
             overflow: 'hidden',
+            zIndex: '2',
+            top: big.top,
+            left: big.left,
+            width: `${width}px`,
+            height: `${height}px`,
             display: isShown ? 'block' : 'none',
-            width: '100px',
-            height: '100px',
             borderRadius: '50%',
             border: '1px solid black'
+        };
+
+        const smallStyle = {
+            position: 'absolute',
+            zIndex: '2',
+            top: small.top,
+            left: small.left
         };
 
         return (
@@ -84,8 +115,8 @@ export default class Loupe extends Component {
                  onMouseEnter={this.onMouseEnter.bind(this)}
                  onMouseLeave={this.onMouseLeave.bind(this)}
                  onMouseMove={this.onMouseMove.bind(this)}>
-               <div className="loupe" style={loupeStyle}>
-                  <img src={image} />
+               <div className="big" style={bigStyle}>
+                  <img className="small" style={smallStyle} src={image} />
                </div>
             </div>
         );

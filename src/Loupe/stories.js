e041b309df5460e7322f427d56d18508a98ff7a2
Walker Randolph Smith <walkerrandolphsmith@gmail.com>
2016-07-06 22:02:44 -0400

    Add a story to show adding class names to the loupe

--- src/Loupe/stories.js
+++ src/Loupe/stories.js
@@ -26,4 +26,16 @@ storiesOf('Loupe', module)
                 borderRadius: '50%'
             }}
         ></Loupe>
+    ))
+    .add('with class names', () => (
+        <Loupe
+            image={'http://www.desktopwallpaper.cn/pic22/adllnorjoxhosoamfdrn.jpg'}
+            classNames={['image', 'image-class']}
+        ></Loupe>
+    ))
+    .add('with loupe class names', () => (
+        <Loupe
+            image={'http://www.desktopwallpaper.cn/pic22/adllnorjoxhosoamfdrn.jpg'}
+            loupeClassNames={['my-loupe', 'loupe-class']}
+        ></Loupe>
     ));

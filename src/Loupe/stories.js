a759191242f527ef14b81bba29172dd0d3ded3cd
Walker Randolph Smith <walkerrandolphsmith@gmail.com>
2016-07-06 21:56:47 -0400

    Add a story for overriding loupe styles

--- src/Loupe/stories.js
+++ src/Loupe/stories.js
@@ -8,13 +8,22 @@ storiesOf('Loupe', module)
             image={'http://www.desktopwallpaper.cn/pic22/adllnorjoxhosoamfdrn.jpg'}
         ></Loupe>
     ))
-    .add('with override styles', () => (
+    .add('with overriden styles', () => (
         <Loupe
             image={'http://www.desktopwallpaper.cn/pic22/adllnorjoxhosoamfdrn.jpg'}
             styles={{
                 border: '1px solid green',
                 height: '250px',
                 width: '220px'
+            }}
+        ></Loupe>
+    ))
+    .add('with overriden loupe styles', () => (
+        <Loupe
+            image={'http://www.desktopwallpaper.cn/pic22/adllnorjoxhosoamfdrn.jpg'}
+            loupeStyles={{
+                border: '1px solid green',
+                borderRadius: '50%'
             }}
         ></Loupe>
     ));

e7c2feb98ae903d43cb36523b0f54bdc010955f3
Walker Randolph Smith <walkerrandolphsmith@gmail.com>
2016-07-06 21:50:39 -0400

    Add a story for overriding the deafult styles

--- src/Loupe/stories.js
+++ src/Loupe/stories.js
@@ -3,6 +3,18 @@ import { storiesOf, action } from '@kadira/storybook';
 import Loupe from './Loupe';
 
 storiesOf('Loupe', module)
-    .add('with no classes', () => (
-        <Loupe image={'http://www.desktopwallpaper.cn/pic22/adllnorjoxhosoamfdrn.jpg'}></Loupe>
+    .add('default', () => (
+        <Loupe
+            image={'http://www.desktopwallpaper.cn/pic22/adllnorjoxhosoamfdrn.jpg'}
+        ></Loupe>
+    ))
+    .add('with override styles', () => (
+        <Loupe
+            image={'http://www.desktopwallpaper.cn/pic22/adllnorjoxhosoamfdrn.jpg'}
+            styles={{
+                border: '1px solid green',
+                height: '250px',
+                width: '220px'
+            }}
+        ></Loupe>
     ));
